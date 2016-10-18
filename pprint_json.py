85a0b6a5959163c37c472bfb96b0333c43182d4a
m.kruglov <m.kruglov@npo-echelon.ru>
2016-10-18 18:24:30 +0300

    New wave of pprint

--- pprint_json.py
+++ pprint_json.py
@@ -12,51 +12,59 @@ def load_data(filepath):
     bars = json.loads(line)
     return bars
 
-def print_with_tab(string, tab_size, tab_char='\t'):
+def print_with_tab(string, tab_size, tab_char='    ', end=True):
     assert type(tab_size) == int and tab_size > 0, 'size should be int and greater that 0'
     assert type(tab_char) == str, 'tab_char should be string and nothing else'
     print(tab_char*tab_size, end='')
-    print(string)
+    if end:
+        print(string)
+    else:
+        print(string, end='')
 
-def pretty_print_json(data, tab_size=1):
-    # tab_size = 1
+def pretty_print_json(data, tab_size=1, suspend_first_tab=False):
     if type(data) == list:
-        print_with_tab('[', tab_size)
-    elif type(data) == dict:
-        print_with_tab('{', tab_size)
-    tab_size += 1
-
-    for item in data:
-        if type(item) == list:
+        if suspend_first_tab:
+            print('[')
+        else:
             print_with_tab('[', tab_size)
-        elif type(item) == dict:
-            print_with_tab('{', tab_size)
-        tab_size += 1
-        for item_key in item:
-            item_value = item[item_key]
-            item_value_type = type(item_value)
-            if item_value_type == int:
-                print_with_tab('"{}" : {}'.format(item_key, item_value), tab_size)
-            elif item_value_type == str:
-                print_with_tab('"{}" : "{}"'.format(item_key, item_value), tab_size)
-            elif item_value_type == list:
-                pass
-            elif item_value_type == dict:
-                pass
+            tab_size += 1
+        for item in data:
+            if type(item) == int:
+                print_with_tab('{}'.format(item), tab_size)
+            elif type(item) == str:
+                print_with_tab('"{}"'.format(item), tab_size)
             else:
-                print('Wooow. Unknown type detected: {}:{} is {}'.format(item_key, item_value, item_value_type))
-            # print_with_tab(item_key, tab_size)
-        tab_size -= 1
-        if type(item) == list:
-            print_with_tab(']', tab_size)
-        elif type(item) == dict:
-            print_with_tab('}', tab_size)
+                print_with_tab('"{}" : '.format(item), tab_size, end=False)
+                pretty_print_json(item, tab_size+1, suspend_first_tab=True)
+            # else:
+            #     print_with_tab('"{}" : null'.format(item), tab_size)
+            # pretty_print_json(item, tab_size+1)
+    elif type(data) == dict:
+        if suspend_first_tab:
+            print('{')
+        else:
+            print_with_tab('{', tab_size)
+            tab_size += 1
+        for item_key in data:
+            item_value = data[item_key]
+            item_type = type(item_value)
+            if item_type == int:
+                print_with_tab('"{}" : {}'.format(item_key, item_value), tab_size)
+            elif item_type == str:
+                print_with_tab('"{}" : "{}"'.format(item_key, item_value), tab_size)
+            elif item_type == dict or item_type == list:
+                print_with_tab('"{}" : '.format(item_key), tab_size, end=False)
+                pretty_print_json(data[item_key], tab_size+1, suspend_first_tab=True)
+            else:
+                print_with_tab('"{}" : null'.format(item_key), tab_size)
 
     tab_size -= 1
     if type(data) == list:
         print_with_tab(']', tab_size)
     elif type(data) == dict:
         print_with_tab('}', tab_size)
+
+
 
 if __name__ == '__main__':
     usage = 'Usage: %prog -p path_to_json'
@@ -80,3 +88,9 @@ if __name__ == '__main__':
         exit(-1)
 
     pretty_print_json(json_data)
+
+
+    # print('==============')
+    # print('{', end='')
+    # # print()
+    # print('==============')
